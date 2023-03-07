b656bd83cb24d8260ebbff66b40dafa33fadb822
robert <robert@unknown020fb576a7ca.attlocal.net>
2023-03-07 15:28:06 -0800

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ut recusandae 
+        vero ducimus amet dolorem magni? Neque animi nam dolor eos cupiditate accusamus? 
+        Accusamus quam, nulla ab excepturi est eligendi.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 
+    {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="grid grid-cols-4 gap-5">
+            <div v-for="p in products">
+                <ProductCard :product="p"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+//fetch the products from API
+
+const { data: products } = await useFetch('https://fakestoreapi.com/products')
+
+definePageMeta({
+    layout:"product"
+})
+
+</script>
+
+<style scoped>
+
+</style>
