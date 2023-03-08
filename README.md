0ea51b54d3f0d0e0f0b410e0fd8d7cdd2401ac2d
robert <robert@unknown020fb576a7ca.attlocal.net>
2023-03-07 22:28:21 -0800

    added custom error page and updated README

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# Nuxt.js and Tailwind CSS store for kids to sell products. 
+# Learning Nuxt.Js - Nuxt.js and Tailwind CSS store for kids to sell products.  
 
 Use Case:
 
 A place for kids to sell their homemade products. The Kidz Store serves as a common place for kids around the world to have their creations showcased and sold.
+
+
+
+# Fixes:
+    -error page: not displaying custom buton. 
--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,25 +1,30 @@
 <template>
-    <div>
-       <ProductDetails :product="product"/>
-    </div>
+  <div>
+    <ProductDetails :product="product" />
+  </div>
 </template>
 
 <script setup>
+const { id } = useRoute().params;
 
-const { id } = useRoute().params
-
-const uri = 'https://fakestoreapi.com/products/' + id
+const uri = "https://fakestoreapi.com/products/" + id;
 
 //fetching the product by id
 
-const { data: product } = await useFetch(uri, {key: id})
+const { data: product } = await useFetch(uri, { key: id });
+
+if (!product.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Sorry... that product is not in our inventory",
+    fatal: true
+    //fatal forces client side errors to show on page
+  });
+}
 
 definePageMeta({
-    layout:"product"
-})
-
+  layout: "product",
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
